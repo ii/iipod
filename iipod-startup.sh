113a2b164eb388561b67b93bb253527ecc3c2220
Hippie Hacker <hh@ii.coop>
2023-08-03 09:10:08 +0200

    Update new-windows to not focus, but just create

--- iipod-startup.sh
+++ iipod-startup.sh
@@ -19,7 +19,7 @@ git remote add ssh $GIT_REPO_SSH
 "
 
 echo "Starting TMUX session: $SPACE_NAME:emacs"
-tmux new-window -t $SPACENAME -n "emacs"
+tmux new-window -d -t $SPACENAME -n "emacs"
 tmux send-keys -t "$SPACENAME:emacs" "
 sleep 15
 cd $REPO_DIR
@@ -33,31 +33,31 @@ echo These windows contain the services supporting your iipod
 "
 
 echo "Starting TMUX session: servers:ttyd"
-tmux new-window -t "servers" -n "ttyd"
+tmux new-window -d -t "servers" -n "ttyd"
 tmux send-keys -t "servers:ttyd" "
 ttyd --writable tmux at -t $SPACENAME
 "
 
 echo "Starting TMUX session: servers:web-server"
-tmux new-window -t "servers" -n "web-server"
+tmux new-window -d -t "servers" -n "web-server"
 tmux send-keys -t "servers:web-server" "
 python3 -m http.server
 "
 
 echo "Starting TMUX session: servers:code-server"
-tmux new-window -t "servers" -n "code-server"
+tmux new-window -d -t "servers" -n "code-server"
 tmux send-keys -t "servers:code-server" "
 code-server --auth none --port 13337
 "
 
 echo "Starting TMUX session: servers:broadwayd"
-tmux new-window -t "servers" -n "broadwayd"
+tmux new-window -d -t "servers" -n "broadwayd"
 tmux send-keys -t "servers:broadwayd" "
 broadwayd :5
 "
 
 echo "Starting TMUX session: servers:emacs-pgtk"
-tmux new-window -t "servers" -n "emacs-pgtk"
+tmux new-window -d -t "servers" -n "emacs-pgtk"
 tmux send-keys -t "servers:emacs-pgtk" "
 export GDK_BACKEND=broadway
 export BROADWAY_DISPLAY=:5
@@ -65,16 +65,16 @@ emacs $ORGFILE
 "
 
 echo "Starting TMUX session: servers:novnc"
-tmux new-window -t "servers" -n "novnc"
+tmux new-window -d -t "servers" -n "novnc"
 tmux send-keys -t "servers:novnc" "
 # We use a branch of novnc that supports OSC52 in Chrome and partially in Firefox
 cp -a /usr/share/novnc ~/novnc
 cp ~/novnc/vnc.html ~/novnc/index.html
-websockify -D --web=/home/ii/novnc 6080 localhost:5901
+websockify --web=/home/ii/novnc 6080 localhost:5901
 "
 
 echo "Starting TMUX session: servers:tigervnc"
-tmux new-window -t "servers" -n "tigervnc"
+tmux new-window -d -t "servers" -n "tigervnc"
 tmux send-keys -t "tigervnc" "
 unset GDK_BACKEND # must not be set when using X
 export PATH=/usr/local/stow/emacs-x/bin:$PATH
